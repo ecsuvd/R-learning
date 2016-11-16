e054c15287d49fd1fa1e625d59c2eb9efdb6e7a3
ecsuvd <echi1100@yahoo.com>
2016-11-16 23:38:58 +0100

    Investigate connection between plane ages and delays
    Taken from the "nycflights13" data.

--- Exercise13.4.6R4DS.R
+++ Exercise13.4.6R4DS.R
@@ -1,15 +1,14 @@
-# Exercise 13.4.6 R4DS: Investigating whether there is a connection between the age of planes and the delay
+#Investigating whether there is a connection between the age of planes and the delay from "nycflights13" data. 
 library(tidyverse)
 library(nycflights13)
 by_age<-group_by(flights, tailnum)
 delay<-summarize(by_age, av_arr_del=mean(arr_delay[arr_delay>0], na.rm=TRUE), av_dep_del=mean(dep_delay[dep_delay>0], na.rm=TRUE))
 delay<-planes %>% 
-  select(tailnum,year) %>%
-  right_join(delay, c("tailnum"="tailnum"))
+select(tailnum,year) %>%
+right_join(delay, c("tailnum"="tailnum"))
 delay<-mutate(delay, age=2013-year)
 ggplot(delay) + 
-  geom_point(aes(age, av_arr_del), na.rm=TRUE, color="blue") + 
-  geom_point(aes(age, av_dep_del), na.rm=TRUE, color="red") + 
-  ylab("delay") +xlab("Age of the plane")
-
-# conclusion: the trend of delay vs the age of the plane if virtually horizantal which is interpretted as there is no (significant) dependency on the delay. 
+    geom_point(aes(age, av_arr_del, color="Arrival"), na.rm=TRUE) + 
+    geom_point(aes(age, av_dep_del, color="Departure"), na.rm=TRUE) + 
+    labs(x="Delay", y="Age of the plane",color="case") 
+#Conclusion: the trend of delay vs age of the plane is virtually horizantal which is interpretted as there is no (significant) dependency on the delay. 
