58f26e0b2e7c44d1f7db2ddd05bb1a0bcd2d604b
ecsuvd <echi1100@yahoo.com>
2017-02-07 22:43:06 +0100

    Update format

--- programming_exercises/Exercise_19.2.1.5.R
+++ programming_exercises/Exercise_19.2.1.5.R
@@ -1,20 +1,14 @@
 #------------------------------
-# Exercise 19.2.1 R4DS: Write both_na(), a function that takes 2 vectors of the same length and 
+# Exercise 19.2.1.5 R4DS: Write both_na(), a function that takes 2 vectors of the same length and 
 # returns the number of positions that have an NA in both vectors.
 #------------------------------
-# x, y vectors of the same length.
-both_na<-function(x,y) {
-  if (length(x)!=length(y)) {
+# x, y are vectors of the same length.
+both_na <- function(x,y) {
+  if (length(x) != length(y)) {
     stop("Vector lengths are not the same.")
   } else {
-    na_location_x<-which(is.na(x))
-    na_location_y<-which(is.na(y))
-    na_location_x[na_location_x==na_location_y]
+    na_location_x <- which(is.na(x))
+    na_location_y <- which(is.na(y))
+    na_location_x[na_location_x == na_location_y]
   }
-}
-# test:
-x=c(NA, 1:4, NA, NA)
-y=c(NA, NA, 4:7, NA)
-z=c(1:5, NA, 7:10)
-both_na(x,y)
-both_na(x,z)  
+}  
--- programming_exercises/Exercise_19.4.4.2.R
+++ programming_exercises/Exercise_19.4.4.2.R
@@ -1,15 +1,15 @@
 #------------------------------
-# Exercise 19.4.4 R4DS: Write a greeting function that says “good morning”, “good afternoon”, or “good evening”, depending on the time of day.   
+# Exercise 19.4.4.2 R4DS: Write a greeting function that says “good morning”, “good afternoon”, or “good evening”, depending on the time of day.   
 #------------------------------
-greeting<-function() {
-  time<-lubridate::now()
-  if (strptime("05:00", "%H:%M")<=time && time<strptime("12:00", "%H:%M")) {
+greeting <- function() {
+  time <- lubridate::now()
+  if (strptime("05:00", "%H:%M") <= time && time < strptime("12:00", "%H:%M")) {
     paste("Good morning!")
-  } else if (strptime("12:00", "%H:%M")<=time && time<strptime("17:00", "%H:%M")) {
+  } else if (strptime("12:00", "%H:%M") <= time && time < strptime("17:00", "%H:%M")) {
       paste("Good afternoon!")
-  } else if (strptime("17:00", "%H:%M")<=time && time<strptime("21:00", "%H:%M")) { 
+  } else if (strptime("17:00", "%H:%M") <= time && time < strptime("21:00", "%H:%M")) { 
       paste("Good evening!")
-  } else if (strptime("21:00", "%H:%M")<=time && time<strptime("05:00", "%H:%M")) {
+  } else if (strptime("21:00", "%H:%M") <= time && time < strptime("05:00", "%H:%M")) {
       paste("Good night!")
   }
 }  
